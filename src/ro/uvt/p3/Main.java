117eed65177ee6a5ec2fda8663eae397758f7e6b
Nanu Andrei <andrei.nanu01@e-uvt.ro>
2022-10-19 17:13:03 +0300

    Main modified

--- src/ro/uvt/p3/Main.java
+++ src/ro/uvt/p3/Main.java
@@ -6,6 +6,7 @@ import ro.uvt.p3.io.OutputDevice;
 public class Main {
 
     public static void main(String[] args) {
+        String New="Modified something";
         InputDevice id = new InputDevice();
         OutputDevice od = new OutputDevice();
         Application app = new Application(id, od);
